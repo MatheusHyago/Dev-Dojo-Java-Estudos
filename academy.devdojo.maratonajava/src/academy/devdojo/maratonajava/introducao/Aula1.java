65a46dec28a2ceb2c4edfba926a6040e05f44bc8
estudos <matheushyago92@gmail.com>
2025-07-07 15:15:22 -0300

    ajustes

--- academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/introducao/Aula1.java
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/introducao/Aula1.java
@@ -1,3 +1,5 @@
+package academy.devdojo.maratonajava.introducao;
+
 public class Aula1 {
     public static void main(String[] args) {
         System.out.println("Olá Marilene!");
---
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/introducao/Aula2.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.introducao;
+public class Aula2 {
+
+    /**
+     *       8 Tipos Primitivos / Mensagens de codigos
+     * ATALHO: psvm + tab(public static void)
+     *
+     *
+     *
+     * Gurdam em memoria valores simples (palavras reservadas)
+     * int, float, double, char, byte, short, long, boolean
+     */
+
+
+    public static void main(String[] args) {
+        /** utilizar camelCase para declarar variaveis maiores ex idadeDoPai */
+        int age = 10;
+
+        /**Atalho sout*/
+
+        /**Imprimir com e sem " "
+         *
+         * quando impresso entre " " imprime valor literal
+         *
+         * quando impresso fora de ex nome da variavel imprime valor alocado em memoria
+         *
+         * exemplo concatenação com println utiliza +
+         * */
+        System.out.println("a idade é " + age);
+    }
+
+
+}
+
