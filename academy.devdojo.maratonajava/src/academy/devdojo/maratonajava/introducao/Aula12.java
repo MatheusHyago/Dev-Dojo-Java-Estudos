153f8e8f1d0c61213b83b9b348689de8a77b4d04
litch <matheushyago92@gmail.com>
2025-07-12 16:41:06 -0300

    exercicios mais teste scaner

---
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/introducao/Aula12.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * <pre>
+ *     While, do While, For
+ *
+ *     Obrigatorio Count
+ * </pre>
+ *
+ * */
+
+public class Aula12  {
+
+    public static void main(String[] args) {
+
+
+        // obrigatorio ser boolean ex count < 1
+        // ctrl + f2 stopa
+        int count = 0;
+
+        while (count < 10) {
+            System.out.println(count);
+            // imprime sem incrementar
+            System.out.println(++count);
+            //incrementa 1 a cada impressao e stopa quando for menor que o definido
+        }
+    }
+
+}
