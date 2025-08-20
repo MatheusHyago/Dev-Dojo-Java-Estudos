8b8f794c281785011ff074d8f6a731df36c09166
litch <matheushyago92@gmail.com>
2025-08-20 20:44:41 -0300

    aulas 50

--- academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Aintroducaometodo/model/Estudante.java
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Aintroducaometodo/model/Estudante.java
@@ -12,6 +12,9 @@ public class Estudante {
      * Imprime os dados do estudante no console.
      * Utiliza 'this' para acessar os atributos do objeto atual.
      */
+
+    // this referencia o objeto atual instanciado dentro da classe para acessar as vairaveis /
+    // atributos locais da classe
     public void imprime() {
         System.out.println("------------------------------");
         System.out.println("Nome: " + this.nome);
