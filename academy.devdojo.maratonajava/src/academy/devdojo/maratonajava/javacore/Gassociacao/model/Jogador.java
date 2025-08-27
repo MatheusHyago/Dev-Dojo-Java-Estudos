aa8070b8204af7f244f032bfd845ca665954dea6
litch <matheushyago92@gmail.com>
2025-08-27 18:22:30 -0300

    aulas 68

--- academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Gassociacao/model/Jogador.java
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Gassociacao/model/Jogador.java
@@ -4,19 +4,37 @@ package academy.devdojo.maratonajava.javacore.Gassociacao.model;
 /**
  * associacao cria mais de uma referencia a variavel instaciada
  *
- * associacao relaciona uma classe com outra // relacionamento de dois objetos um pra um
+ * associacao relaciona uma classe com outra
+ * unidirecional = relacionamento de dois objetos um pra um jogador tem um time
+ * bidirecional = relacionamento de dois objetos um para muitos jogador tem varios times
+ *
  */
 public class Jogador {
 
     private String nome;
+    private Time time;
 
     public Jogador(String nome) {
         this.nome = nome;
     }
 
-
     public void imprime() {
+
         System.out.println("Nome: " + this.nome);
+
+        if(time != null) {
+            System.out.println("Time: " + getTime().getNome());
+        } else {
+            System.out.println("O jogador não tem time");
+        }
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
     }
 
     public String getNome() {
---
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Gassociacao/test/EscolaTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.model.Escola;
+import academy.devdojo.maratonajava.javacore.Gassociacao.model.Professor;
+
+public class EscolaTest01 {
+    public static void main(String[] args) {
+
+        Professor professor = new Professor("Mahoraga");
+
+        Professor [] professores = {professor};
+        Escola escola = new Escola("Sombras", professores);
+
+        escola.imprime();
+    }
+}
