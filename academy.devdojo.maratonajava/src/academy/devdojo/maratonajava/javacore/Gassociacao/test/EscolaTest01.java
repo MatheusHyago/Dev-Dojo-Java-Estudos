7237ce8adf1936560d0557ae33c0cf38c56ba46c
litch <matheushyago92@gmail.com>
2025-08-27 19:56:32 -0300

    aulas 68

--- academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Gassociacao/test/EscolaTest01.java
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Gassociacao/test/EscolaTest01.java
@@ -6,7 +6,7 @@ import academy.devdojo.maratonajava.javacore.Gassociacao.model.Professor;
 public class EscolaTest01 {
     public static void main(String[] args) {
 
-        Professor professor = new Professor("Mahoraga");
+        Professor professor = new Professor("Mahoraga", "Matemática");
 
         Professor [] professores = {professor};
         Escola escola = new Escola("Sombras", professores);
