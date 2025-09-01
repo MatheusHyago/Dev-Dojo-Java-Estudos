c1754170558298d2e0798d7b368467936b2582fa
litch <matheushyago92@gmail.com>
2025-09-01 18:27:42 -0300

    heranca

--- academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Hherenca/test/HerancaTest02.java
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/javacore/Hherenca/test/HerancaTest02.java
@@ -6,9 +6,17 @@ public class HerancaTest02 {
     public static void main(String[] args) {
         // em herança a ordem de execução é
 
-        //primeiro a superclasse
-        //depois a subclasse
-        //blocos de inicialização estáticos são executados na ordem que aparecem
+
+        //0 - Bloco de inicialização estático da superclasse é executado quando a JVM carregar a classe superclasse
+        //1 - Bloco de inicialização estático da subclasse é executado quando a JVM carregar a classe subclasse
+        //2 - Aloca espaço na memória para o objeto da superclasse
+        //3 - Cada atributo de superclasse é inicializado com valores default ou que quer que tenha sido definido na classe
+        //4 - Bloco de inicialização da superclasse é executado na ordem que aparece na classe
+        //5 - Construtor da superclasse é executado
+        //6 - Aloca espaço na memória para o objeto da subclasse
+        //7 - Cada atributo de subclasse é inicializado com valores default ou que quer que tenha sido definido na classe
+        //8 - Bloco de inicialização da subclasse é executado na ordem que aparece na classe
+        //9 - Construtor da subclasse é executado
         Funcionario funcionario = new Funcionario("Gustavo");
 
     }
