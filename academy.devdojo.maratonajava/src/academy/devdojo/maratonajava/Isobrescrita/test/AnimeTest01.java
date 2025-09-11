4a64eff747e6cca3b863901dc5a4977ab724c2cb
litch <matheushyago92@gmail.com>
2025-09-11 20:13:36 -0300

    heranca

---
+++ academy.devdojo.maratonajava/src/academy/devdojo/maratonajava/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.Isobrescrita.model.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+
+
+    Anime anime = new Anime("Death Note");
+
+
+
+        System.out.println(anime);
+
+    }
+}
